ed54a4893da3e2ac708a76c3e4abb0185d8fbdac
Zachary Lentz <zlentz@slac.stanford.edu>
2024-07-12 15:56:27 -0700

    FIX: typo recieve -> receive

--- afs_ioc_migration/lock_repo.py
+++ afs_ioc_migration/lock_repo.py
@@ -8,7 +8,7 @@ class AlreadyLockedError(RuntimeError): ...
 
 def lock_file_repo(path: str, org: str) -> None:
     """
-    Add a pre-recieve hook to a filepath repo to prevent further pushes.
+    Add a pre-receive hook to a filepath repo to prevent further pushes.
 
     The intention is to run this prior to the migration so that no new
     changes are introduced during the move to github.
@@ -19,10 +19,10 @@ def lock_file_repo(path: str, org: str) -> None:
         raise ValueError(
             f"{path} is not a valid git repo, it has no hooks subdirectory."
         )
-    pre_receive_path = hooks_dir / "pre-recieve"
+    pre_receive_path = hooks_dir / "pre-receive"
     if pre_receive_path.exists():
         raise AlreadyLockedError(
-            f"There is already a pre-recieve hook at {pre_receive_path}."
+            f"There is already a pre-receive hook at {pre_receive_path}."
         )
 
     template_path = Path(__file__).parent / "hook_template.txt"
