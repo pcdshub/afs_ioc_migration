35f60f3c0a283433db6c4d5c0bf07594aee2bf2d
Zachary Lentz <zlentz@slac.stanford.edu>
2024-07-12 15:36:05 -0700

    FIX: forgot to keep this to the ioc path

--- run_afs_to_pcdshub.sh
+++ run_afs_to_pcdshub.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m afs_ioc_migration --org pcdshub --dry-run /afs/slac.stanford.edu/g/cd/swe/git/repos/package/epics/*/*
+python -m afs_ioc_migration --org pcdshub --dry-run /afs/slac.stanford.edu/g/cd/swe/git/repos/package/epics/ioc/*/*
