ca355a8ce82a5e71e3a3fd57a3a3cd593187208d
Zachary Lentz <zlentz@slac.stanford.edu>
2024-07-15 16:16:58 -0700

    ENH: automatically tee output to a logfile

--- run_afs_to_pcdshub.sh
+++ run_afs_to_pcdshub.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m afs_ioc_migration --org pcdshub --dry-run /afs/slac.stanford.edu/g/cd/swe/git/repos/package/epics/ioc/*/*.git /afs/slac.stanford.edu/g/cd/swe/git/repos/package/epics/ioc/xpp/ccm/*.git
+python -m afs_ioc_migration --org pcdshub --dry-run /afs/slac.stanford.edu/g/cd/swe/git/repos/package/epics/ioc/*/*.git /afs/slac.stanford.edu/g/cd/swe/git/repos/package/epics/ioc/xpp/ccm/*.git | tee migration.log
