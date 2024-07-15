51482915d022a76475abd7d44ccc05d48eb56523
Zachary Lentz <zlentz@slac.stanford.edu>
2024-07-15 16:29:53 -0700

    FIX: bad tea

--- run_afs_to_pcdshub.sh
+++ run_afs_to_pcdshub.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m afs_ioc_migration --org pcdshub --dry-run /afs/slac.stanford.edu/g/cd/swe/git/repos/package/epics/ioc/*/*.git /afs/slac.stanford.edu/g/cd/swe/git/repos/package/epics/ioc/xpp/ccm/*.git | tee migration.log
+python -m afs_ioc_migration --org pcdshub --dry-run /afs/slac.stanford.edu/g/cd/swe/git/repos/package/epics/ioc/*/*.git /afs/slac.stanford.edu/g/cd/swe/git/repos/package/epics/ioc/xpp/ccm/*.git 2>&1 | tee -a migration.log
